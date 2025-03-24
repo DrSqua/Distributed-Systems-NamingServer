1eacd2f38e82c04252b1d806dde78b8d182c7e58
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-03-24 16:31:47 +0100

    Initial commit

---
+++ src/test/java/schnitzel/distributedsystems/DistributedSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package schnitzel.distributedsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
