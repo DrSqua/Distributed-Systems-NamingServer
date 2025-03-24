51aad79e3129a40be7664124509deda76fe213be
runesleeuwaert <runesleeuwaert@hotmail.com>
2025-03-24 17:09:44 +0100

    Implemented the file mapping functionality between json and the program.

--- src/main/java/schnitzel/NamingServer/FileMapping.java
+++ src/main/java/schnitzel/NamingServer/FileMapping.java
@@ -1,10 +1,30 @@
 package schnitzel.NamingServer;
 
-public class InputFile {
-    String filename;
-    // TODO body
-}
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileMapping {
-    String filename;
+    private static final String FILE_PATH = "map.json";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void saveMapToJson(Map<Integer, Integer> map) {
+        try {
+            objectMapper.writeValue(new File(FILE_PATH), map);
+            System.out.println("Map saved to " + FILE_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Map<Integer, Integer> loadMapFromJson() {
+        try {
+            return objectMapper.readValue(new File(FILE_PATH), Map.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
