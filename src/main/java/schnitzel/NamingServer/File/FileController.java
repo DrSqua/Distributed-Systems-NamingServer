8aac990c4901e94380a37b09755ad5286234b48f
runesleeuwaert <runesleeuwaert@hotmail.com>
2025-03-26 17:56:43 +0100

    Made the Hash what it is in the slides, added implementation of methods to add and delete files to the map but still need a way to get the nodes ip address

--- src/main/java/schnitzel/NamingServer/File/FileController.java
+++ src/main/java/schnitzel/NamingServer/File/FileController.java
@@ -3,6 +3,9 @@ package schnitzel.NamingServer.File;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.InetAddress;
+import java.util.Map;
+
+import static java.lang.Math.abs;
 
 @RestController
 public class FileController {
@@ -30,22 +33,28 @@ public class FileController {
     // Get Unique
     @GetMapping("/file/{fileName}")
     FileMapping get(@PathVariable String fileName) {
-        Integer fileHash = fileName.length(); // TODO, better hash
+        Integer fileHash = (fileName.hashCode() + 2147483647) * (32768/2147483647 +2147483647);
         return repository.findByFileHash(fileHash);
     }
 
     // Create New
     @PostMapping("/file")
     FileMapping newFile(@RequestBody InputFile newFile) {
-        // TODO
-        // HASH File and store in repository
-
-        return null; // TODO repository.save(accountBalance);
+        Integer IP_node = null; // TODO get nodes IP address
+        Integer fileHash = (newFile.hashCode() + 2147483647) * (32768/2147483647 + 2147483647);
+        Map<Integer, Integer> map = FileMapping.loadMapFromJson();
+        map.put(fileHash, IP_node);
+        FileMapping.saveMapToJson(map);
+        return null;
     }
 
     // Delete
     @DeleteMapping("/file/{fileName}")
     void delete(@PathVariable String fileName) {
-        // TODO Delete file from repository
+        Integer IP_node = null; // TODO get nodes IP address
+        Integer fileHash = (fileName.hashCode() + 2147483647) * (32768/2147483647 + 2147483647);
+        Map<Integer, Integer> map = FileMapping.loadMapFromJson();
+        map.remove(fileHash);
+        FileMapping.saveMapToJson(map);
     }
 }
