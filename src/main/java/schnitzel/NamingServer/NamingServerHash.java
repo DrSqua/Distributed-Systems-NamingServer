f5331080e5a62f40f3fee446833d2bd6ac730c0e
Tobias2512 <hagenaarstobias@gmail.com>
2025-04-02 17:22:32 +0200

    Still wrong hashCode but now its finally fixed xD

--- src/main/java/schnitzel/NamingServer/NamingServerHash.java
+++ src/main/java/schnitzel/NamingServer/NamingServerHash.java
@@ -5,7 +5,6 @@ public class NamingServerHash {
     public static Long hash(String nodeIdentifier) {
         long max = 2147483647;
         long hashCode = nodeIdentifier.hashCode();
-        double helper = (double) 32768/max + max;
-        return (long) ((hashCode + max) * helper);
+        return ((hashCode + max) * 32768) / (2*max);
     }
 }
