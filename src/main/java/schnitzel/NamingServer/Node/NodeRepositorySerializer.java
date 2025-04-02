693bb518d4d689eab970b88280cf22928b664868
runesleeuwaert <runesleeuwaert@hotmail.com>
2025-04-02 17:21:09 +0200

    Added a way to save a map with Hash and IP of each node to a json file.

---
+++ src/main/java/schnitzel/NamingServer/Node/NodeRepositorySerializer.java
@@ -0,0 +1,39 @@
+package schnitzel.NamingServer.Node;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.print.DocFlavor;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class NodeRepositorySerializer {
+
+    private static final String BACKUP_FILE = "node_repository_backup.json";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private NodeRepository nodeRepository;
+
+    // Save all entities to JSON file
+    public void saveToJson() {
+        List<NodeEntity> nodes = nodeRepository.findAll();
+        Map<Long, String> nodeMap = new HashMap<>();
+
+        for (NodeEntity node : nodes) {
+            nodeMap.put(node.getNodeHash(), node.getIpAddress());
+        }
+
+        try {
+            objectMapper.writeValue(new File(BACKUP_FILE), nodeMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
