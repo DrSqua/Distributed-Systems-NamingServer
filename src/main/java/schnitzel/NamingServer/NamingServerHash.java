33056dc93018e73a526caeb9523e319892a32699
Tobias2512 <hagenaarstobias@gmail.com>
2025-04-23 17:06:58 +0200

    made constant value final

--- src/main/java/schnitzel/NamingServer/NamingServerHash.java
+++ src/main/java/schnitzel/NamingServer/NamingServerHash.java
@@ -3,7 +3,7 @@ package schnitzel.NamingServer;
 public class NamingServerHash {
 
     public static Long hash(String nodeIdentifier) {
-        long max = 2147483647;
+        final long max = 2147483647;
         long hashCode = nodeIdentifier.hashCode();
         return ((hashCode + max) * 32768) / (2*max);
     }
