52aac6e08a8cfc4e27dc52a785ab31da409d4ec2
Mateo Van Geyt <mateo.vangeyt@gmail.com>
2025-04-25 00:31:25 +0200

    server now responds to new nodes on the network

--- src/main/java/Utilities/Multicast.java
+++ src/main/java/Utilities/Multicast.java
@@ -8,8 +8,10 @@ import schnitzel.NamingServer.Node.NodeStorageService;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class Multicast {
@@ -17,7 +19,6 @@ public class Multicast {
     private static int PORT;
     private static MulticastSocket socket;
     private static String IP;
-    //TODO: storage
     private final NodeStorageService storage;
 
     public Multicast(String IP, String groupIP, int port) throws IOException {
@@ -95,6 +96,17 @@ public class Multicast {
                 Long hash = NamingServerHash.hash(nodeName);
                 NodeEntity node = new NodeEntity(nodeIP,hash, nodeName);
                 storage.put(hash,node);
+                /*
+                 *  Now the namingServer will respond. By sending the number of existing nodes to the new node.
+                 *  We made this a multicast, because this number needs to be updated by all nodes (otherwise
+                 *  they will have an incorrect number).
+                 */
+                String response = String.valueOf(storage.count());
+                byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
+                DatagramSocket responseSocket = new DatagramSocket();
+                DatagramPacket responsePacket = new DatagramPacket(responseBytes, responseBytes.length, packet.getAddress(), packet.getPort());
+                responseSocket.send(responsePacket);
+                responseSocket.close();
             }
         } catch (IOException e){
             e.printStackTrace();
