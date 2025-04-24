cd889a710feedf03959736d41c24226c32a4d092
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-04-24 23:44:32 +0200

    Commented unimplemented code

--- src/main/java/NodeClient/Node.java
+++ src/main/java/NodeClient/Node.java
@@ -6,17 +6,17 @@ public class Node {
 
     }
 
-    private synchronized void updateNextNodeIP(){
-        if(nextNodeIp == null || nextNodeIp.equals("") || nextNodeIp.isEmpty()){
-            getNextIp();
-        }
-    }
-
-    private void getNextIp() {
-        try {
-            helpMethods.sendUnicast("Requesting IP from ID", serverIP, "GET_IP_FROM_ID" + ":" + IP , Ports.unicastPort);
-        } catch (Exception e) {
-            logger.log(Level.WARNING, "Unable to get IP from ID", e);
-        }
-    }
+//    private synchronized void updateNextNodeIP(){
+//        if(nextNodeIp == null || nextNodeIp.equals("") || nextNodeIp.isEmpty()){
+//            getNextIp();
+//        }
+//    }
+//
+//    private void getNextIp() {
+//        try {
+//            helpMethods.sendUnicast("Requesting IP from ID", serverIP, "GET_IP_FROM_ID" + ":" + IP , Ports.unicastPort);
+//        } catch (Exception e) {
+//            logger.log(Level.WARNING, "Unable to get IP from ID", e);
+//        }
+//    }
 }
