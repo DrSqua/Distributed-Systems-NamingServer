d612d1ba9b8fbbd931088a71529ed110f481d1cc
Tobias2512 <hagenaarstobias@gmail.com>
2025-05-25 20:04:49 +0200

    Added original file owner get REST method + moved some methods from NodeController to FileController

--- src/main/java/schnitzel/NamingServer/File/FileController.java
+++ src/main/java/schnitzel/NamingServer/File/FileController.java
@@ -1,14 +1,13 @@
 package schnitzel.NamingServer.File;
 
 import Utilities.NodeEntity.NodeEntity;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import schnitzel.NamingServer.NamingServerHash;
 import schnitzel.NamingServer.Node.NodeStorageService;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static java.lang.Math.abs;
 
@@ -22,8 +21,11 @@ public class FileController {
     }
 
     private final NodeStorageService nodeStorageService;
-    FileController(NodeStorageService nodeStorageService) {
+    private final FileOriginalOwnerService fileOriginalOwnerService;
+
+    FileController(NodeStorageService nodeStorageService, FileOriginalOwnerService fileOriginalOwnerService) {
         this.nodeStorageService = nodeStorageService;
+        this.fileOriginalOwnerService = fileOriginalOwnerService;
     }
 
     /**
@@ -48,5 +50,73 @@ public class FileController {
         }
         NodeEntity locatedNode = this.nodeStorageService.findById(locatedNodeHash).orElseThrow();
         return locatedNode.getIpAddress();
+    }
+
+    /**
+     *
+     * @param fileHash: Hash value of the file
+     * @return the owner of the given file
+     */
+    @GetMapping("/file/owner")
+    public NodeEntity getFileOwner(@RequestParam long fileHash) {
+        NodeEntity owner = findResponsibleNode(fileHash);
+        if (owner == null) {
+            throw new FileController.ResourceNotFoundException("Node with hash " + fileHash + " does not exist");
+        }
+        return owner;
+    }
+    /**
+     * @param fileHash: Hash value of the file
+     * @return the original (first) owner of the given file
+     */
+    @GetMapping("/file/original-owner")
+    public NodeEntity getOriginalFileOwner(@RequestParam long fileHash) {
+        NodeEntity originalOwner = fileOriginalOwnerService.getOriginalFileOwner(fileHash);
+        if (originalOwner == null) {
+            throw new FileController.ResourceNotFoundException("Node with hash " + fileHash + " does not exist");
+        }
+        return originalOwner;
+    }
+
+    @PostMapping("/file/register-original-owner")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void registerOriginalOwner(@RequestParam long fileHash, @RequestParam long originalNodeHash) {
+        boolean success = fileOriginalOwnerService.registerOriginalFileOwner(fileHash, originalNodeHash);
+        if (!success) {
+            throw new FileController.ResourceNotFoundException("Couldn't register original owner!");
+        }
+    }
+
+    /**
+     * @param fileHash: Hash value of the file
+     * @param nodeHash: Hash value of the originating node
+     * @return Informs originating node that he needs to replicate or ignore the file
+     */
+    @GetMapping("/file/replication")
+    public String checkReplicationResponsibility(@RequestParam long fileHash, @RequestParam long nodeHash) {
+        NodeEntity responsibleNode = findResponsibleNode(fileHash);
+
+        assert responsibleNode != null;
+        if (responsibleNode.getNodeHash().equals(nodeHash)) {
+            return "REPLICATE";
+        }
+        return "IGNORE";
+    }
+
+    private NodeEntity findResponsibleNode(long fileHash) {
+        ArrayList<Long> sortedHashes = nodeStorageService.keys();
+        if (sortedHashes.isEmpty()) {
+            return null;
+        }
+        // sort the hashes so the smallest node hash is first
+        Collections.sort(sortedHashes);
+        for (Long nodeHash : sortedHashes) {
+            // if the fileHash is smaller, then the node has this node is the owner
+            if (fileHash <= nodeHash) {
+                return nodeStorageService.findById(nodeHash).orElse(null);
+            }
+        }
+        // Wrap around, fileHash is less than all node hashes -> smallest nodeHash owns it
+        return nodeStorageService.findById(sortedHashes.get(0)).orElse(null);
     }
 }
