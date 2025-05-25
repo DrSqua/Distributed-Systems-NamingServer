40a8efcbbb8c1807e0a7eb69cb4e37b2c63510b6
Tobias2512 <hagenaarstobias@gmail.com>
2025-05-25 20:11:35 +0200

    Moved transferReplicatedFiles to a static method in Shutdown.java, so we can access it on more places

--- src/main/java/NodeClient/Components/OnShutdownBean.java
+++ src/main/java/NodeClient/Components/OnShutdownBean.java
@@ -6,9 +6,9 @@ import NodeClient.File.FileService;
 import NodeClient.RingAPI.RingStorage;
 import Utilities.NodeEntity.NodeEntity;
 import Utilities.RestMessagesRepository;
+import Utilities.Shutdown;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import schnitzel.NamingServer.NamingServerHash;
 
@@ -43,7 +43,7 @@ public class OnShutdownBean {
         RestMessagesRepository.removingSelfFromSystem(this.ringStorage.getSelf(), this.ringStorage.getNamingServerIP(), previousNode, nextNode);
         try {
             handleLocalFiles();
-            transferReplicatedFiles();
+            Shutdown.transferReplicatedFiles(this.fileService, this.ringStorage);
         }
         catch (IOException e) {
             System.out.println("Could not handle local files or transfer replicated files, continuing shutdown process...");
@@ -62,7 +62,7 @@ public class OnShutdownBean {
             String fileName = file.getName();
             long fileHash = NamingServerHash.hash(fileName);
             // log a shutdown operation
-            fileLoggerService.logOperation(fileName, fileHash, "SHUTDOWN", file.getPath());
+            fileLoggerService.logOperation(fileName, fileHash, "SHUTDOWN", ringStorage.currentName(), file.getPath());
             boolean wasDownloaded = fileLoggerService.wasFileDownloaded(fileName);
             // if the file has been downloaded we transfer the file to our neighbor and then delete the file here
             if (wasDownloaded) {
@@ -71,50 +71,10 @@ public class OnShutdownBean {
                 );
                 FileMessage message = new FileMessage(fileName, "TRANSFER_LOCAL", Files.readAllBytes(file.toPath()));
                 RestMessagesRepository.handleTransfer(message, previousNode.getIpAddress());
-
             }
             // now we can safely delete the file
             FileMessage message = new FileMessage(fileName, "DELETE_LOCAL", Files.readAllBytes(file.toPath()));
             fileService.handleFileOperations(message);
-        }
-    }
-
-    private void transferReplicatedFiles() throws IOException {
-        File[] files = Paths.get("replicated_files").toFile().listFiles();
-        NodeEntity currentNode = RestMessagesRepository.getNode(this.ringStorage.currentName(), this.ringStorage.getNamingServerIP());
-
-        if (files == null) {
-            return;
-        }
-        for (File file : files) {
-            if (!file.isFile()) {
-                continue;
-            }
-            String fileName = file.getName();
-            long fileHash = NamingServerHash.hash(fileName);
-            NodeEntity ownerNode = RestMessagesRepository.getFileOwner(fileHash, ringStorage.getNamingServerIP());
-            NodeEntity ownerPrev = RestMessagesRepository.getNeighbor(ownerNode, "PREVIOUS");
-            NodeEntity ownerNext = RestMessagesRepository.getNeighbor(ownerNode, "NEXT");
-            // Check here if we need to replicate the file to next or previous node
-            NodeEntity targetNode;
-            // if current node is the owner's previous node, we have to replicate the file to current node's previous node
-            if (currentNode.getNodeHash().equals(ownerPrev.getNodeHash())) {
-                targetNode = RestMessagesRepository.getNeighbor(ownerPrev, "PREVIOUS");
-            }
-            // if current node is the owner's next node, we have to replicate the file to current node's next node
-            else if (currentNode.getNodeHash().equals(ownerNext.getNodeHash())) {
-                targetNode = RestMessagesRepository.getNeighbor(ownerNext, "NEXT");
-            }
-            else {
-                continue;
-            }
-            byte[] data = Files.readAllBytes(file.toPath());
-            // first transfer to the target node
-            FileMessage transferMessage = new FileMessage(fileName, "TRANSFER_REPLICA", data);
-            RestMessagesRepository.handleTransfer(transferMessage, targetNode.getIpAddress());
-            // then delete the file
-            FileMessage deleteMessage = new FileMessage(fileName, "DELETE_REPLICA", null);
-            fileService.handleFileOperations(deleteMessage);
         }
     }
 }
